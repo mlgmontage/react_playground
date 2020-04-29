4d3eb5bda6700067b5b4002d0c15b005d932e24f
Abay <kettleofrassel1@gmail.com>
2020-04-29 07:12:02 -0700

    setting up

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Message from "./components/Message";
+import Calculator from "./components/Calculator";
 
 function App() {
   return (
     <div className="App">
       <Message msg="this is message" />
+      <Calculator />
     </div>
   );
 }
---
+++ src/components/Calculator.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class Calculator extends React.Component {
+  render() {
+    return (
+      <div>
+        <h3>Calculator</h3>
+      </div>
+    );
+  }
+}
+
+export default Calculator;
